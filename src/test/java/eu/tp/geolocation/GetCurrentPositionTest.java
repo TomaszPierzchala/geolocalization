e1f7f2ddaf693b0878397331513504c823c5e755
TomaszPierzchala <t.pierzchala@gmail.com>
2018-02-07 16:15:15 +0100

    mock file for netsh (Windows) changed, test modified

--- src/test/java/eu/tp/geolocation/GetCurrentPositionTest.java
+++ src/test/java/eu/tp/geolocation/GetCurrentPositionTest.java
@@ -39,18 +39,25 @@ class GetCurrentPositionTest {
 	@Test
 	void testPickUpWifisScannerResultsAtWindows() throws IOException {
 		
-		Path file = FileSystems.getDefault().getPath( "src/test/java", "eu/tp/geolocation", "netsh_mock.txt");
+		Path file = FileSystems.getDefault().getPath( "src/test/java", "eu/tp/geolocation", "natsh_wlan_show_networks_mode_bssid_mock.txt");
 //		System.out.println(file.toAbsolutePath());
 		InputStream stream = new ByteArrayInputStream(Files.readAllBytes(file));
 		List<WifiData> resultList = GetCurrentPosition.pickUpWifiDataAtWindows(stream);
 		stream.close();
 		
 		List<WifiData> expectedList = new LinkedList<WifiData>();
+		expectedList.add(new WifiData("ac:d2:13:d0:12:50", "72",  "4"));
+		expectedList.add(new WifiData("fa:60:37:34:a6:aa", "44",  "1"));
+		expectedList.add(new WifiData("aa:21:00:01:02:1d", "40",  "11"));
+		expectedList.add(new WifiData("ee:bb:cc:82:cc:12", "49",  "13"));
+		expectedList.add(new WifiData("a1:20:c8:d0:11:ab", "72",  "4"));
+		expectedList.add(new WifiData("a2:67:13:79:8b:da", "28",  "11"));
+		expectedList.add(new WifiData("30:72:ac:6c:01:a3", "14",  "145"));
 		expectedList.add(new WifiData("44:aa:f5:aa:11:b3", "16",  "1"));
 		expectedList.add(new WifiData("ac:22:05:83:cc:10", "64",  "11"));
 		expectedList.add(new WifiData("a1:27:d7:45:df:89", "62",  "11"));
-		expectedList.add(new WifiData("ae:22:15:d0:10:57", "72",  "4"));
-		expectedList.add(new WifiData("de:cb:0a:fe:12:23", "90",  "4"));
+		expectedList.add(new WifiData("ae:22:15:d0:10:57", "72",  "7"));
+		expectedList.add(new WifiData("de:cb:0a:fe:12:23", "90",  "47"));
 		
 		assertEquals(expectedList.toString(), resultList.toString());
 	}
